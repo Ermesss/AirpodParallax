51e9a079e1a2e10ca34ed01c63bd17d025a4a263
Ermanno <ermannofissore@gmail.com>
2022-11-30 17:32:26 +0000

    Initial Commit

---
+++ AirpodParallax/AirpodParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirpodParallaxApp.swift
+//  AirpodParallax
+//
+//  Created by Ermanno Fissore on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AirpodParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
